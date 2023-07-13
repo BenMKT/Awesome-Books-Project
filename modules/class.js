83e1036e0c13325113d95906e50c80f3f574d93d
Benson Kiai <benson.kiai86@gmail.com>
2023-07-13 18:55:01 +0300

    Refactored Book class to solve removebtn bug

--- modules/class.js
+++ modules/class.js
@@ -1,24 +1,35 @@
 import { bookSection, titleText, authorText } from "./book.js";
-
 export default class Book {
   constructor() {
     this.bookLibrary = JSON.parse(localStorage.getItem("jsonLibrary")) || [];
-  }
-
+    }
   insertHtml() {
-    bookSection.innerHTML = "";
+    bookSection.replaceChildren();
     this.bookLibrary.forEach((n) => {
-      bookSection.innerHTML += `<div class='book'>
-        <div class='bookDetail'>
-          <p>"${n.title}"</p>
-          <p>by</p>
-          <p>${n.author}</p>
-        </div>    
-          <button id="${n.id}" class='remove' type='button'>Remove</button>
-      </div>`;
+      const divContainer = document.createElement("div");
+      divContainer.classList.add("book");
+      const divDetail = document.createElement("div");
+      divDetail.classList.add("bookDetail");
+      const pTitle = document.createElement("p");
+      pTitle.innerHTML = n.title;
+      const pBy = document.createElement("p");
+      pBy.innerHTML = "By";
+      const pAuthor = document.createElement("p");
+      pAuthor.innerHTML = n.author;
+      const btnRemove = document.createElement("button");
+      btnRemove.classList.add("remove");
+      btnRemove.type = "button";
+      btnRemove.innerHTML = "Remove";
+      btnRemove.addEventListener("click", () => {
+        this.remove(n.id);
+      });
+      divDetail.appendChild(pTitle);
+      divDetail.appendChild(pBy);
+      divDetail.appendChild(pAuthor);
+      divContainer.appendChild(divDetail);
+      divContainer.appendChild(btnRemove);
+      bookSection.appendChild(divContainer);
     });
-
-    
   }
 
   addBook() {
@@ -39,5 +50,4 @@ export default class Book {
     this.insertHtml();
   }
 }
-
 
